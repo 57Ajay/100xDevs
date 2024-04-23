897ae9462389079288f858a3d0e7c2cd10b4df81
Ajay Upadhyay <57ajay.u@gmail.com>
2024-04-23 22:57:07 +0530

    Understanding Wrappers

--- week6/src/App.jsx
+++ week6/src/App.jsx
@@ -1,9 +1,32 @@
 import './App.css';
+import PropTypes from 'prop-types';
 
+const TextComponent = ()=>{
+  return(
+    <h1>Hi! there</h1>
+  );
+};
+const CardWrapper = ({children})=>{
+
+  return (
+    <div className = "cardWrapper">
+      {children}
+    </div>
+  );
+};
+
+CardWrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 const App = ()=>{
   return(
-    <div>
-      <h1>Everything`s ok!</h1>
+    <div >
+      <CardWrapper>
+        <h1>Ajay</h1>
+      </CardWrapper>
+      <CardWrapper>
+        <TextComponent text = "Aman"/>
+      </CardWrapper>      
     </div>
   );
 };
