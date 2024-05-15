7a0ac551509f5eaff38ae0847e0f436e98a02662
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-16 00:36:34 +0530

    minor changes

--- Week-7/src/components7.3/Renderer.jsx
+++ Week-7/src/components7.3/Renderer.jsx
@@ -2,17 +2,31 @@ import { Fragment } from "react"
 import './Renderer.css'
 import {  useRecoilValue } from "recoil";
 import { NetworkAtom, jobsAtom, messagingAtom, notificationsAtom } from "./atoms/atoms";
+import PropTypes from 'prop-types';
+
 const  Renderer=()=> {
-    const netoworkNotificationCount = useRecoilValue(NetworkAtom);
+    const networkNotificationCount = useRecoilValue(NetworkAtom);
+    const jobsNotificationCount = useRecoilValue(jobsAtom);
+    const messagingNotificationCount = useRecoilValue(messagingAtom);
+    const notificationsNotificationCount = useRecoilValue(notificationsAtom);
+
+    const ValueRenderer = ({value})=>{
+        const val = value>=99 ? "99+" : value; 
+        return <span>{val}</span>
+    };
+    ValueRenderer.propTypes = {
+        value: PropTypes.number.isRequired,
+    };
+    
   return (
     <Fragment>
         <div className="full-recoil-container">
             <button>Home</button>
 
-            <button>My Network</button>
-            <button>Jobs</button>
-            <button>Messaging</button>
-            <button>Notifications</button>
+            <button>My Network({<ValueRenderer value={networkNotificationCount}/>})</button>
+            <button>Jobs({<ValueRenderer value={jobsNotificationCount}/>})</button>
+            <button>Messaging({<ValueRenderer value={messagingNotificationCount}/>})</button>
+            <button>Notifications({<ValueRenderer value={notificationsNotificationCount} />})</button>
 
             <button>Me</button>
         </div>
