04912aff4af1d5c113d1d458bb8cb4cfa47a4605
Ajay Upadhyay <140410149+57Ajay@users.noreply.github.com>
2024-05-15 17:00:54 +0530

    another wat to add EvenSelector into Count.js

--- Week-7/src/components7.2/Store/Atoms/Count.js
+++ Week-7/src/components7.2/Store/Atoms/Count.js
@@ -1,7 +1,15 @@
-import { atom } from 'recoil';
+import { atom /*,selector*/ } from 'recoil';
 const countAtom = atom({
     key: "countAtom",
     default: 0
 });
 
 export default countAtom;
+// Another ay of creating EvenSelector;
+// export const EvenSelector = selector({
+//     key: "EvenSelector",
+//     get: ({ get }) => {
+//         const count = get(countAtom);
+//         return count % 2 === 0;
+//     }
+// });
