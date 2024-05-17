fe87f0eb4a1e6fc79555f554e8bb4dff7e3d97df
Ajay Upadhyay <140410149+57Ajay@users.noreply.github.com>
2024-05-17 22:52:30 +0530

    Added async approach to fetch data

--- Week-7/src/components7.3/atoms/atoms.js
+++ Week-7/src/components7.3/atoms/atoms.js
@@ -9,3 +9,28 @@ export const notificationsAtom = atom({
     },
 })
 
+/*
+
+To fetch data form the backend using async function
+export const notificationsAtom = ({
+    key: "notifications",
+    default: selector({
+        key: "notificationsSelectors",
+        get: async()=>{
+            const res = await axios.get("https://server.com/notifications");
+            return res.data;
+
+        }
+    });
+});
+Data model example from backend  ======>
+
+{ 
+    network: 102,
+    jobs: 0,
+    notifications: 12,
+    messaging: 0
+}
+
+
+*/
