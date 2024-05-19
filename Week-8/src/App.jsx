163261376f9ed1bb1412aacde4359254adbfd7d4
Ajay Upadhyay <57ajay.u@gmail.com>
2024-05-19 09:07:18 +0530

    fixed minor error

--- Week-8/src/App.jsx
+++ Week-8/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Fragment } from 'react';
+import RevenueCard from './components/RevenueCard';
 
 const App=()=> {
   return (
     <Fragment>
-      <RevenueCard />
-    </Fragment>
+      <RevenueCard title={"Amount Pending"} amount={`22,345,423`} orderCount={10}/>
+    </Fragment >
   )
 }
 
